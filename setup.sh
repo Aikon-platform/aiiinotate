293ed006928a04dc476a3dae77b7861343839fc2
paulhectork <paul.kervegan@hotmail.fr>
2025-08-29 18:10:12 +0200

    [SETUP] setup.sh fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "./scripts/utils.sh";
 
-source "$SCRIPT_DIR/scripts/utils.sh";
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 color_echo blue "\nInstalling prompt utility fzy..."
 if [ "$OS" = "Linux" ]; then
--- src/app.test.js
+++ src/app.test.js
@@ -5,8 +5,8 @@
 import build from "#src/app.js";
 
 async function test() {
-    const fastify = await build({});
-    console.log(fastify);
+  const fastify = await build({});
+  console.log(fastify);
 }
 
 test();
