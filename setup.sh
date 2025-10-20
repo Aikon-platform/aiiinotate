bb8b2e949a801bcf86585d85722c35828f0c187e
paulhectork <paul.kervegan@hotmail.fr>
2025-10-20 16:26:44 +0200

    [SCRIPT] fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 source "./scripts/utils.sh";
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 color_echo blue "\nInstalling prompt utility fzy..."
 if [ "$OS" = "Linux" ]; then
