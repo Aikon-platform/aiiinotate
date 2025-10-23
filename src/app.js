3598176c7daad33f16fa96b825b8f4ccc05a7742
paulhectork <paul.kervegan@hotmail.fr>
2025-10-23 21:05:09 +0200

    attempt to add fastify swagger

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+// import swagger from "@fastify/swagger";
 
 import fileServer from "#fileServer/index.js";
 import schemas from "#schemas/index.js";
@@ -33,7 +34,50 @@ const defaultConfig = {
   mongo: { }
 }
 
-const allowedModes = ["test", "default"]
+//NOTE: couldn´t get fastify/swagger to work for now...
+// const swaggerConfig = {
+//   openapi: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Aiiinotate",
+//       description: "A fast and lightweight IIIF annotations server",
+//       version: "1.0.0",
+//     },
+//     servers: [
+//       {
+//         url: process.env.APP_BASE_URL,
+//         description: "Aiiinotate URL"
+//       }
+//     ],
+//     // tags: [],
+//     // components: {},
+//     // externalDocs: {
+//     //   url: 'https://swagger.io',
+//     //   description: 'Find more info here'
+//     // }
+//   },
+//
+//   // swagger: {
+//   //   info: {
+//   //     title: "Aiiinotate",
+//   //     description: "A fast and lightweight IIIF annotations server",
+//   //     version: "1.0.0",
+//   //   },
+//   //   externalDocs: {
+//   //     url: 'https://swagger.io',
+//   //     description: 'Find more info here'
+//   //   },
+//   //   host: process.env.APP_BASE_URL.replace(/^http(s)?\:\/\//g, ""),  // process.env.APP_BASE_URL,
+//   //   schemes: [ "http", "https" ],
+//   //   consumes: ['application/json'],
+//   //   produces: ['application/json'],
+//   //   tags: [ Object ],
+//   // },
+//   hideUntagged: false,
+//   exposeRoute: true,
+// }
+
+const allowedModes = ["test", "default"];
 
 /**
  * @param {"test"|"default"} mode
