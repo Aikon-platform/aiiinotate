1f52b58641064141ff57b38f91a739672b5ee419
paulhectork <paul.kervegan@hotmail.fr>
2026-02-04 15:59:30 +0100

    [UTILS] bugfix isObject

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,7 +20,7 @@ const isNullOrUndefined = (v) => v == null;
 const isNullish = (v) => v == null || !v.length;
 
 /** o is an object but not an array. https://stackoverflow.com/a/44556453 */
-const isObject = (o) => o.constructor === Object;
+const isObject = (o) => o?.constructor === Object;
 
 const isNonEmptyArray = (a) => Array.isArray(a) && a.length;
 
